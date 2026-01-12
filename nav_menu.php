3b5a615ffbe865aed1887ee4c5518e75691b12fc
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2026-01-12 13:32:26 +0200

    navigation on uuendatud

---
+++ nav_menu.php
@@ -0,0 +1,9 @@
+<nav class="menu">
+    <ul>
+        <li><a href="registration.php">Registration</a></li>
+        <li><a href="slaalom.php">Slaalom</a></li>
+        <li><a href="ringtee.php">Ringtee</a></li>
+        <li><a href="tanav.php">Tänavasõit</a>
+        <li><a href="lubadeleht.php">Lubadeleht</a>
+    </ul>
+</nav>

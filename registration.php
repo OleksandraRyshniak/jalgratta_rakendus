923fa22f9b010fd94be5483eb12ce305f1b9eb65
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2026-01-12 14:51:21 +0200

    lisatud nimi ja perekonnanime kontroll

--- registration.php
+++ registration.php
@@ -1,12 +1,29 @@
 <?php
 require_once("config.php");
 global $connect;
-if(isSet($_REQUEST["sisestusnupp"])){
-    $kask=$connect->prepare(
-        "INSERT INTO jalgrattaeksam(eesnimi, perekonnanimi) VALUES (?, ?)");
-    $kask->bind_param("ss", $_REQUEST["eesnimi"], $_REQUEST["perekonnanimi"]);
-    $kask->execute();
-    $connect->close();
+if (!empty($_POST["sisestusnupp"])) {
+    if (isset($_POST["eesnimi"])) {
+        $eesnimi = trim($_POST["eesnimi"]);
+    } else {
+        $eesnimi = '';
+    }
+
+    if (isset($_POST["perekonnanimi"])) {
+        $perekonnanimi = trim($_POST["perekonnanimi"]);
+    } else {
+        $perekonnanimi = '';
+    }
+
+    if ($eesnimi === '' || is_numeric($eesnimi)) {
+        echo "Sisesta oma eesnimi!";
+    } elseif ($perekonnanimi === '' || is_numeric($perekonnanimi)) {
+        echo "Sisesta oma perekonnanimi!";
+    } else {
+        $stmt = $connect->prepare("INSERT INTO jalgrattaeksam (eesnimi, perekonnanimi) VALUES (?, ?)");
+        $stmt->bind_param("ss", $eesnimi, $perekonnanimi);
+        $stmt->execute();
+        $connect->close();
+    }
 }
 ?>
 <!doctype html>

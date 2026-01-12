dc7e8c4e1d2b7800704236bab240bbc564ceac70
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2026-01-12 15:50:09 +0200

    funktsioon teooriaeksaam on lisatud

--- funktsioonid.php
+++ funktsioonid.php
@@ -9,3 +9,37 @@ function lisaKasutaja($eesnimi, $perekonnanimi)
     $stmt->execute();
     $stmt->close();
 }
+
+function teooriatulemus($id, $teooriatulemus)
+{
+    global $connect;
+    $stmt = $connect->prepare("UPDATE jalgrattaeksam SET teooriatulemus=? WHERE id=?");
+    $stmt->bind_param("ii", $teooriatulemus, $id);
+    $stmt->execute();
+    $stmt->close();
+}
+
+function naitaTabel()
+{
+    global $connect;
+    $paring = $connect->prepare("SELECT id, eesnimi, perekonnanimi FROM jalgrattaeksam WHERE teooriatulemus<10");
+    $paring->bind_result($id, $eesnimi, $perekonnanimi);
+    $paring->execute();
+    while($paring->fetch()){
+    echo "<table>";
+    echo "<tr>
+    <td>$eesnimi</td>
+    <td>$perekonnanimi</td>
+    <td>
+    <form method='post' action=''>
+    <input type='hidden' name='id' value='$id' />
+    <input type='text' name='teooriatulemus' />
+    <input type='submit' value='Sisesta tulemus' />
+    </form>
+    </td>
+    </tr>";
+    echo "</table>";}
+    $paring->close();
+}
+
+
--- teooriaeksam.php
+++ teooriaeksam.php
@@ -1,14 +1,10 @@
 <?php
-require_once("config.php");
-global $connect;
-if(!empty($_REQUEST["teooriatulemus"])){
-    $kask=$connect->prepare(
-        "UPDATE jalgrattaeksam SET teooriatulemus=? WHERE id=?");
-    $kask->bind_param("ii", $_REQUEST["teooriatulemus"], $_REQUEST["id"]); $kask->execute();
+require_once("funktsioonid.php");
+if (isset($_REQUEST['teooriatulemus'])){
+    teooriatulemus($_REQUEST['id'], $_REQUEST['teooriatulemus']);
+    header("Location:". $_SERVER['PHP_SELF']);
+    exit;
 }
-$kask=$connect->prepare("SELECT id, eesnimi, perekonnanimi   FROM jalgrattaeksam WHERE teooriatulemus=-1");
-$kask->bind_result($id, $eesnimi, $perekonnanimi);
-$kask->execute();
 ?>
 <!doctype html>
 <html>
@@ -22,28 +18,12 @@ include("header.php");
 include("nav_menu.php");
 ?>
 <main>
-    <h2>Teooria eksam</h2>
-<table>
-    <?php
-    while($kask->fetch()){
-        echo " 
- <tr> 
- <td>$eesnimi</td> 
- <td>$perekonnanimi</td> 
- <td><form action=''> 
- <input type='hidden' name='id' value='$id' /> 
- <input type='text' name='teooriatulemus' />
- <input type='submit' value='Sisesta tulemus' /> 
- </form> 
- </td> 
-</tr> 
- ";
-    }
-    ?>
-</table>
+<h2>Teooria eksam</h2>
+<?php
+naitaTabel();
+?>
 </main>
 <?php
-//jalus
 include("footer.php");
 ?>
 </body>
